8cf4ffd1653b73996597a1464eceac2ab2798b12
VincentBott <vincentbott@gmail.com>
2018-08-09 09:11:43 +0200

    Werkende versie van de docent, Jef.

--- src/com/company/Cursus.java
+++ src/com/company/Cursus.java
@@ -1,29 +1,31 @@
 package com.company;
 
-private int aantalCursisten;
+
+//  Werkende versie van de docent, Jef.
 
 
 public class Cursus {
-
-
     private static final int MAXCURSISTEN = 6;
-    private String[] cursisten = new String[MAXCURSISTEN];
-
+    private String[] cursisten = new String [MAXCURSISTEN];
+    private int aantalCursisten=0;
 
     public void addCursist(String naam) {
-    }
+        if (zitCursusVol()) return;
+        cursisten[aantalCursisten] = naam;
+        aantalCursisten++;
 
-    public int getAantalCursisten() {
-        return 0;
     }
 
     public boolean zitCursusVol() {
-        if (aantalCursisten == MAXCURSISTEN)
-            return true;
-        else
-            return false;
+        return MAXCURSISTEN == aantalCursisten;
     }
 
-    public String getCursist(int i) {
+    public String getCursist(int index) {
+        if (index < 1 || index > MAXCURSISTEN)return null;
+        return cursisten[index-1];
+    }
+
+    public int getAantalCursisten() {
+        return aantalCursisten;
     }
 }
